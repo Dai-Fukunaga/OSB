6e368812ca3696d73b771552b542267db668b3ef
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2024-01-14 16:31:13 +0900

    modify myWrite

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
     /* <fd, "name:flag:flag:"> */
 
     public static void main(String[] args) {
+        /*stdin,stdout,stderr */
         MyFile file1 = new MyFile("stdin:O_RDONLY:", null); /*stdinからのreadは未実装 */
         fd_dict.put(0, file1);
         File f2 = new File("/dev/pts/0");
@@ -15,37 +16,37 @@ public class Main {
         File f3 = new File("/dev/pts/0");
         MyFile file3 = new MyFile("stderr:O_WRONLY:", f3); /*stderrへのwriteはできる */
         fd_dict.put(2, file3);
-        int i = 0;
-        if (myClose(2) == -1){
-            System.err.println("close err");
-        }
-        i = myOpen("abc.txt",MyFlags.O_RDONLY | MyFlags.O_CREAT);
-        System.out.println("fd = " + i);
-        i = myOpen("abc.txt",MyFlags.O_RDONLY | MyFlags.O_CREAT);
-        System.out.println("fd = " + i);
 
-        if (myClose(i) == -1){
-            System.err.println("close err");
-        }
-        i = myOpen("abc.txt",MyFlags.O_RDWR | MyFlags.O_CREAT);
+        int i = 0;
+        //if (myClose(2) == -1){
+        //    System.err.println("close err");
+        //}
+        //i = myOpen("abc.txt",MyFlags.O_RDONLY);
+        //System.out.println("fd = " + i);
+        //i = myOpen("abc.txt",MyFlags.O_RDONLY);
+        //System.out.println("fd = " + i);
+
+        //if (myClose(i) == -1){
+        //    System.err.println("close err");
+        //}
+        i = myOpen("abc.txt",MyFlags.O_RDWR);
         System.out.println("fd = " + i);
         String[] buf = new String[] {""}; /*参照渡しのためlistに */
-        if (myRead(i, buf,3) != 3){
-            System.err.println("read err");
-        }
-        System.out.println("buf = " + buf[0]);
+        //if (myRead(i, buf,3) != 3){
+        //    System.err.println("read err");
+        //}
+        //System.out.println("buf = " + buf[0]);
 
         buf[0] = "hoge";
-        if (myWrite(i, buf, buf[0].length()) != 4){
+        if (myWrite(i, buf,buf[0].length()+1 ) != buf[0].length()+1){
             System.err.println("write err");
         }
     }
 
     public static int myOpen(String name,int flags) {
         /* String name is const */
-        /* mode is 8進数 3桁 */
 
-        /* 2進数6桁埋めで */
+        /* flagsを2進数6桁埋めで */
         String flags_bin = Integer.toBinaryString(flags);
         flags_bin = String.format("%6s",flags_bin).replace(" ","0");
         int flags_bin_len = flags_bin.length();
@@ -146,7 +147,7 @@ public class Main {
             flags[i-1] = info[i];
         }
         if (flags[0].equals("O_WRONLY") == true) {
-            System.err.println("this file is not readable");
+            System.err.println(" Bad file descriptor");
             return -1;
         }
 
@@ -174,6 +175,7 @@ public class Main {
 
     public static int myWrite (int fd , String[] buf, int nbytes) {
         /* String buf is const */
+        String tmp_buf = buf[0];
         if (fd_dict.get(fd) == null) {
             System.err.println("not found fd = " + fd);
             return -1;
@@ -187,20 +189,68 @@ public class Main {
             flags[i-1] = info[i];
         }
         if (flags[0].equals("O_RDONLY") == true) {
-            System.err.println("this file is not writable");
+            System.err.println(" Bad file descriptor");
             return -1;
+        }
+        boolean F_append = false;
+        if ((flags.length == 2 && flags[1].equals("O_APPEND")) == true) {
+            F_append = true;
         }
 
         File f = fd_dict.get(fd).file;
         try{
-            FileWriter fw = new FileWriter(f);
-            if (nbytes > buf[0].length()){
-                System.err.println("nbytes > buf[0].length");
-                fw.write(buf[0]);
-            } else if (nbytes < buf[0].length()) {
-                fw.write(buf[0].substring(0, nbytes));
+            FileWriter fw;
+            if (F_append) {
+                fw = new FileWriter(f, true);
+                if (nbytes > tmp_buf.length()){
+                    System.err.println("nbytes > buf[0].length");
+                    fw.write(tmp_buf);
+                } else if (nbytes < tmp_buf.length()) {
+                    fw.write(tmp_buf, 0, nbytes);
+                } else {
+                    fw.write(tmp_buf);
+                }
             } else {
-                fw.write(buf[0]);
+                int add_bytes = 0;
+                try{
+                    /* 上書きする文字をnbytes分だけにする （例）String buf[0] = "xyz", nbytes = 2 --> buf[0] = "xy" */
+                    /*                                   String buf[0] = "xyz", nbytes = 5 --> buf[0] = "xyz??" */
+                    if (nbytes < tmp_buf.length()){
+                        tmp_buf = tmp_buf.substring(0, nbytes);
+                    } else if (nbytes > tmp_buf.length()){
+                        System.err.println("nbytes > buf[0].length");
+                        for (int i=0; i<(nbytes+add_bytes)-tmp_buf.length(); i++) {
+                        tmp_buf += (char)0;
+                        }
+                    }
+                    FileReader fr = new FileReader(f);
+                    int ch;
+                    /* 上書きされる部分を空読み (例) 元のfile内容 "abcdefghi", nbytes = 3 --> "123"の部分を空読み */
+                    for (int i=0; i<nbytes; i++) {
+                        ch = fr.read();
+                        if (ch == -1) {
+                            break;
+                        }
+                    }
+                    /* 元の部分の読み込み (例) 元のfile内容 "abcdefghi", nbytes = 3 --> "defghi"の部分を読み込み */
+                    while((ch = fr.read()) != -1){
+                        tmp_buf += (char)ch;
+                        add_bytes++;
+                    }
+                    fr.close();
+                } catch (IOException e) {
+                    System.err.println(e);
+                    return -1;
+                }
+                /* write */
+                fw = new FileWriter(f);
+                if (nbytes+add_bytes > tmp_buf.length()){
+                    System.err.println("OH!! This error should not happen!!");
+                } else if (nbytes+add_bytes < tmp_buf.length()) {
+                    fw.write(tmp_buf, 0, nbytes+add_bytes);
+                } else {
+                    fw.write(tmp_buf);
+                }
             }
             fw.close();
         } catch (IOException e) {
